0864435d8a83075a31c880de98aede58e3054c3c
Josh Elser <josh.elser@gmail.com>
2016-12-04 00:17:12 -0500

    Add some more debug to the script and don't auto-background it

--- speakerpower-setup.sh
+++ speakerpower-setup.sh
@@ -6,19 +6,25 @@ VIRTUALENV_BASENAME="venv"
 VIRTUALENV="${INSTALL_DIR}/${VIRTUALENV_BASENAME}"
 
 if [[ ! -d ${INSTALL_DIR} ]]; then
+    echo "Cloning repository"
     git clone ${REPOSITORY} ${INSTALL_DIR}
 else
+    echo "Updating repository"
     pushd ${INSTALL_DIR}
     git fetch && git reset --hard origin/master
     popd
 fi
 
 if [[ ! -d ${VIRTUALENV} ]]; then
+    echo "Installing virtualenvironment"
     pushd $INSTALL_DIR
     virtualenv2 ${VIRTUALENV_BASENAME}
     ./${VIRTUALENV_BASENAME}/bin/pip install flask RPi.GPIO
     popd
+else
+    echo "Virtualenvironment already set up"
 fi
 
+echo "Starting application"
 cd ${INSTALL_DIR}
-FLASK_APP=speakerpower.py ./${VIRTUALENV_BASENAME}/bin/flask run --host=0.0.0.0 --port=80 > /var/log/speakerpower.log 2>&1 &
+FLASK_APP=speakerpower.py ./${VIRTUALENV_BASENAME}/bin/flask run --host=0.0.0.0 --port=80 >> /var/log/speakerpower.log 2>&1
