402b5428c7f0d3401390da80a3053535f2fb9a41
Josh Elser <josh.elser@gmail.com>
2016-12-04 00:07:14 -0500

    A script to bootstrap and start the service

---
+++ speakerpower-setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+REPOSITORY="https://github.com/joshelser/speakerpower-flask.git"
+INSTALL_DIR="/usr/local/speakerpower-flask.git"
+VIRTUALENV_BASENAME="venv"
+VIRTUALENV="${INSTALL_DIR}/${VIRTUALENV_BASENAME}"
+
+if [[ ! -d ${INSTALL_DIR} ]]; then
+    git clone ${REPOSITORY} ${INSTALL_DIR}
+else
+    pushd ${INSTALL_DIR}
+    git fetch && git reset --hard origin/master
+    popd
+fi
+
+if [[ ! -d ${VIRTUALENV} ]]; then
+    pushd $INSTALL_DIR
+    virtualenv2 ${VIRTUALENV_BASENAME}
+    ./${VIRTUALENV_BASENAME}/bin/pip install flask RPi.GPIO
+    popd
+fi
+
+cd ${INSTALL_DIR}
+FLASK_APP=speakerpower.py ./${VIRTUALENV_BASENAME}/bin/flask run --host=0.0.0.0 --port=80 > /var/log/speakerpower.log 2>&1 &
